1aa0730c4eff8b825f24db1252d6ba3cace3993d
vetalz <vitaly482@gmail.com>
2021-09-13 11:03:21 +0300

    template finish

--- solar_panel/urls.py
+++ solar_panel/urls.py
@@ -3,4 +3,10 @@ from solar_panel.views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('contact/', contact, name='contact'),
+    path('product/', product, name='product'),
+    path('ses-home/', ses_home, name='ses_home'),
+    path('ses-business/', ses_business, name='ses_business'),
+    path('product/single/', single_product, name='single_product'),
+    path('green-rate/', green_rate, name='green_rate')
 ]
--- solar_panel/views.py
+++ solar_panel/views.py
@@ -10,3 +10,48 @@ def index(request):
     }
     return render(request, 'solar_panel/index.html', context)
 
+
+def contact(request):
+    context = {
+        'title': 'Контакты',
+    }
+    return render(request, 'solar_panel/contact.html', context)
+
+
+def product(request):
+    products = Product.objects.all()
+    context = {
+        'products': products,
+        'title': 'Наши проекты',
+    }
+    return render(request, 'solar_panel/product.html', context)
+
+
+def single_product(request):
+    products = Product.objects.all()
+    context = {
+        'products': products,
+        'title': 'Конкретный проект'
+    }
+    return render(request, 'solar_panel/single_product.html', context)
+
+
+def ses_home(request):
+    context = {
+        'title': 'Электростанции для дома'
+    }
+    return render(request, 'solar_panel/ses_home.html', context)
+
+
+def ses_business(request):
+    context = {
+        'title': 'Электростанции для бизнеса'
+    }
+    return render(request, 'solar_panel/ses_business.html', context)
+
+
+def green_rate(request):
+    context = {
+        'title': 'Зелёный тариф'
+    }
+    return render(request, 'solar_panel/green_rate.html', context)
